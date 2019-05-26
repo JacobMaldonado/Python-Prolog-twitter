9d0e43c1b77da08944830c44fe8229b13e1a7062
YeicobXD <jacobmaldonado99@gmail.com>
2019-05-26 17:16:23 -0500

    add: response to twitter page

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/webhook/twitter", methods=["POST"])
+@app.route("/webhook/twitter", methods=["GET","POST"])
 def manejador():
     if request.method == "POST":
         print(request.get_json())
