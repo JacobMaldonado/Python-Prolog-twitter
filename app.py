8e1c4756912c4ad58df755c91073339592b46677
YeicobXD <jacobmaldonado99@gmail.com>
2019-05-28 08:37:16 -0500

    add: pyswip

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 from flask import Flask, request, jsonify
+from pyswip import Prolog
 
 app = Flask(__name__)
+CATEGORIAS = ["movil", "desarrollo_web", "programacion", "hardware"]
 
 @app.route("/webhook/twitter", methods=["GET","POST"])
 def manejador():
     if request.method == "POST":
-        print(request.get_json())
+        json = request.get_json()
+        peticion = json["queryResult"]["queryText"]
+        palabras = peticion.split()
+        respuesta = ""
+        if palabras[0].lower() == "cursos" or palabras[0].lower() == "curso":
+            if palabras[1] in CATEGORIAS:
+                p = Prolog()
+                p.consult("cursos.pl")
+                for solution in p.query("curso(X,Y,Z," + palabras[1] +")"):
+                    respuesta += "curso: " + solution["X"] + " instuctor: " + solution["Y"] + " plataforma: " + solution["Z"]
+            else:
+                respuesta = "no se encontro la categoria"
+        else:
+            respuesta = "No entendi tu pregunta, trata escribiendo \"cursos\" y una categoria"
+        return jsonify({"fulfillmentText": respuesta});
     return "hi"
---
+++ cursos.pl
@@ -0,0 +1,28 @@
+instructor(hector_costa).
+instructor(desarrolloWebcom).
+instructor(devcode).
+instructor(eduonix).
+instructor(edumobile_academy).
+instructor(jaureguialzo_sarasola).
+instructor(juan_de_la_torre).
+instructor(codigo_facilito).
+instructor(ismael_garcia).
+instructor(fernanda_ochoa).
+instructor(enrique_hernandez).
+
+categoria(programacion).
+categoria(movil).
+categoria(desarrollo_web).
+categoria(hardware).
+
+curso(introduccion_a_la_programacion_con_pseudocodigo_practico,hector_costa, udemy, programacion).
+curso(fundamentos_de_la_programacion, desarrolloWebcom, youtube, programacion).
+curso(curso_basico_de_android, devcode, devcode, movil).
+curso(learn_html5_programming_from_scratch, eduonix, udemy, desarrollo_web).
+curso(c_programming_for_beginners, edumobile_academy, udemy, programacion).
+curso(fundamentos_de_programacion_con_java, jaureguialzo_sarasola, udemy, programacion).
+curso(cookies_y_sesiones_en_php, juan_de_la_torre, udemy, desarrollo_web ).
+curso(primeros_pasos_android, codigo_facilito, codigo_facilito, movil).
+curso(curso_gratuito_de_flask, ismael_garcia, codigo_facilito, desarrollo_web).
+curso(curso_gratuito_de_arduino, fernanda_ochoa, codigo_facilito, hardware).
+curso(introduccion_al_uso_de_la_terminal, enrique_hernandez, codigo_facilito, programacion).
